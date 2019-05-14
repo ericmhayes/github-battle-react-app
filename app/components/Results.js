00ec7be4cd099a17828bac2a8a92bb1619cf5fad
= <erichayes8512@gmail.com>
2019-05-14 11:53:43 -0500

    refactored files

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,12 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
-function Profile (props) {
-  var info = props.info;
+const React = require('react');
+const PropTypes = require('prop-types');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
+function Profile ({ info }) {
   return (
     <PlayerPreview username={info.login} avatar={info.avatar_url}>
       <ul className='space-list-items'>
@@ -26,12 +25,13 @@ function Profile (props) {
 Profile.propTypes = {
   info: PropTypes.object.isRequired,
 }
-function Player (props){
+
+function Player ({ label, score, profile }) {
   return (
     <div>
-      <h1 className='header'>{props.label}</h1>
-      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-      <Profile info={props.profile}/>
+      <h1 className='header'>{label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+      <Profile info={profile} />
     </div>
   )
 }
@@ -41,55 +41,47 @@ Player.propTypes = {
   score: PropTypes.number.isRequired,
   profile: PropTypes.object.isRequired,
 }
-class Results extends React.Component{
 
-  constructor(props){
+class Results extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       winner: null,
       loser: null,
       error: null,
-      loading:true
+      loading: true,
     }
   }
+  componentDidMount() {
+    const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
 
-  componentDidMount(){
-    var players = queryString.parse(this.props.location.search);
     api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ]).then(function(results){
-      if (results === null){
-        return this.setState(function (){
-            return {
-              error: 'Looks like there was a error. Check that both users exists on Github',
-              loading: false,
-            }
-        });
+      playerOneName,
+      playerTwoName
+    ]).then((players) => {
+      if (players === null) {
+        return this.setState(() => ({
+          error: 'Looks like there was an error. Check that both users exist on Github.',
+          loading: false,
+        }))
       }
 
-      this.setState(function () {
-        return {
-          error: null,
-          winner: results[0],
-          loser: results[1],
-          loading: false
-        }
-      })
-    }.bind(this));
+      this.setState(() => ({
+        error: null,
+        winner: players[0],
+        loser: players[1],
+        loading: false,
+      }));
+    });
   }
   render() {
+    const { error, winner, loser, loading } = this.state;
 
-    var error = this.state.error;
-    var winner = this.state.winner;
-    var loser = this.state.loser;
-    var loading = this.state.loading;
-
-    if(loading === true){
+    if (loading === true) {
       return <Loading />
     }
 
-    if(error){
+    if (error) {
       return (
         <div>
           <p>{error}</p>
@@ -97,6 +89,7 @@ class Results extends React.Component{
         </div>
       )
     }
+
     return (
       <div className='row'>
         <Player
@@ -104,7 +97,6 @@ class Results extends React.Component{
           score={winner.score}
           profile={winner.profile}
         />
-
         <Player
           label='Loser'
           score={loser.score}
